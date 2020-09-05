cf082a4ef953b53ff8f1e0bdd4c94d07deb67a1f
Ryan Awad <61163520+Ryan-Awad@users.noreply.github.com>
2020-09-05 18:58:16 -0400

    Added registration function and encryption)

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterForm
+import json
+import hashlib
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'd18bc10be13889abf8b7e9c9a2ab8e8d'
+
+def encrypt_md5(string): # MD5 encryption for passwords
+    string = string.encode()
+    hash_object = hashlib.md5(string)
+    hash_digest = hash_object.hexdigest()
+    return hash_digest
+
 
 @app.route("/")
 @app.route("/home")
@@ -15,9 +26,32 @@ def create():
 def about():
     return render_template("aboutpage.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    reg_form = RegisterForm()
+    if reg_form.validate_on_submit():
+        # Encrypting the password
+        password = encrypt_md5(reg_form.password.data)
+
+        account_data = {
+            "first_name": reg_form.first_name.data,
+            "last_name": reg_form.last_name.data,
+            "email": reg_form.email.data,
+            "password": password,
+        }
+
+        db_read = json.load(open('database/accounts.json', 'r'))
+        db_read.append(account_data)
+        db = open('database/accounts.json', 'w')
+        json.dump(db_read, db, indent=4)
+
+        return redirect(url_for('home'))
+
+    return render_template("register.html", form=reg_form)
+
+@app.route("/login")
+def login():
+    return "<h1>Login Page</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
